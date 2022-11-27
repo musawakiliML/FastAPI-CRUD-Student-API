172d89e05b1a6329fd2fb463071bcbf508df04ef
musawakiliML <musawakiliml@hotmail.com>
2022-11-27 10:41:40 +0100

    Created student routes

--- app/server/routes/student.py
+++ app/server/routes/student.py
@@ -1,1 +1,19 @@
 from fastapi import APIRouter, Body
+from fastapi.encoders import jsonable_encoder
+
+from app.server.database import (
+    add_student,
+    delete_student,
+    retrieve_student,
+    retrieve_students,
+    update_student,
+)
+
+from app.server.models.student import(
+    ErrorResponseModel,
+    ResponseModel,
+    StudentSchema,
+    UpdateStudentSchema,
+)
+
+router = APIRouter()
