2e9b20ee4bafeb47578c8c3ab86a3010ca245856
musawakiliML <musawakiliml@hotmail.com>
2023-03-10 13:11:17 +0100

    Update database.py

--- app/server/database.py
+++ app/server/database.py
@@ -19,7 +19,7 @@ def student_helper(student) -> dict:
         "id": str(student["_id"]),
         "fullname": student["fullname"],
         "email": student["email"],
-        "course_of_study": student["course"],
+        "course_of_study": student["course_of_study"],
         "year": student["year"],
         "gpa": student["gpa"],
     }
